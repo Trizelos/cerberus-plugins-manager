4be42d91669cbd08d2b6b670769216d2be31f81d
casey <casey.reuter@trizelos.com>
2023-06-23 15:41:31 +0200

    fix: version number

--- cerberus-plugins-manager.php
+++ cerberus-plugins-manager.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Cerberus Plugins Manager
  * Description:       Manages the cerberus plugins
- * Version:           1.0.5
+ * Version:           1.0.6
  * Requires at least: 5.7
  * Author:            Casey
  */
