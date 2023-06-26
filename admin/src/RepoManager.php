f6a02aa82e5c87891b3a084159d701d2f7412363
caseyreuter <108796446+caseyreuter@users.noreply.github.com>
2023-06-26 08:18:44 +0200

    fix: repo install (#8)
    * fix: repo install
    
    * feat: made plugins manager updateable

--- admin/src/RepoManager.php
+++ admin/src/RepoManager.php
@@ -4,80 +4,79 @@ namespace Cerberus\AdminPluginsManager;
 
 class RepoManager
 {
-	private string $username;
-	private string $authorizeToken;
+    private string $username;
+    private string $authorizeToken;
 
-	public function __construct()
-	{
-		$this->username       = 'Trizelos';
-		$this->authorizeToken = get_option( 'repo-key' );
-	}
+    public function __construct()
+    {
+        $this->username = 'Trizelos';
+        $this->authorizeToken = get_option('repo-key');
+    }
 
-	public function getRepoList(): array
-	{
-		$repos = [];
-		foreach ( $this->getRepos() as $repo ) {
-			$name      = $repo['name'];
-			$repoInfos = $this->getRepoInfoByRepoName( $name );
-			if ( empty( $repoInfos ) ) {
-				continue;
-			}
+    public function getRepoList(): array
+    {
+        $repos = [];
+        foreach ($this->getRepos() as $repo) {
+            $name = $repo['name'];
+            $repoInfos = $this->getRepoInfoByRepoName($name);
 
-			$pluginFile = WP_PLUGIN_DIR . '/' . $name . '/' . $name . '.php';
-			$pluginData = get_plugin_data( $pluginFile );
+            if (empty($repoInfos)) {
+                $repoInfos = "";
+            }
 
-			$repos[ $name . '/' . $name . '.php' ]['name']       = $name;
-			$repos[ $name . '/' . $name . '.php' ]['repoInfos']  = $repoInfos;
-			$repos[ $name . '/' . $name . '.php' ]['pluginData'] = $pluginData;
-		}
+            $pluginFile = WP_PLUGIN_DIR . '/' . $name . '/' . $name . '.php';
+            $pluginData = get_plugin_data($pluginFile);
 
-//		pre( $repos );
+            $repos[$name . '/' . $name . '.php']['name'] = $name;
+            $repos[$name . '/' . $name . '.php']['repoInfos'] = $repoInfos;
+            $repos[$name . '/' . $name . '.php']['pluginData'] = $pluginData;
+        }
 
-		return $repos;
-	}
+        return $repos;
+    }
 
-	private function getRepos(): array
-	{
-		$request_uri = sprintf( 'https://api.github.com/orgs/%s/repos', $this->username ); // Build URI
-		$responses   = $this->getResponse( $request_uri );
+    private function getRepos(): array
+    {
+        $request_uri = sprintf('https://api.github.com/orgs/%s/repos', $this->username); // Build URI
+        $responses = $this->getResponse($request_uri);
 
-		if ( ! is_array( $responses ) || empty( $responses[0]['name'] ) ) {
-			return [];
-		}
+        if (!is_array($responses) || empty($responses[0]['name'])) {
+            return [];
+        }
 
-		$repos = [];
-		foreach ( $responses as $response ) {
-			if ( ! str_contains( $response['name'], 'cerberus' ) ) {
-				continue;
-			}
-			$repos[] = $response;
-		}
+        $repos = [];
+        foreach ($responses as $response) {
+            if (!str_contains($response['name'], 'cerberus')) {
+                continue;
+            }
+            $repos[] = $response;
+        }
 
-		return $repos;
-	}
+        return $repos;
+    }
 
-	private function getResponse( string $request_uri ): mixed
-	{
-		$args = array();
+    private function getResponse(string $request_uri): mixed
+    {
+        $args = array();
 
-		if ( $this->authorizeToken ) { // Is there an access token?
-			$args['headers']['Authorization'] = "bearer {$this->authorizeToken}"; // Set the headers
-		}
+        if ($this->authorizeToken) { // Is there an access token?
+            $args['headers']['Authorization'] = "bearer {$this->authorizeToken}"; // Set the headers
+        }
 
-		$response = json_decode( wp_remote_retrieve_body( wp_remote_get( $request_uri, $args ) ), true ); // Get JSON and parse it
+        $response = json_decode(wp_remote_retrieve_body(wp_remote_get($request_uri, $args)), true); // Get JSON and parse it
 
-		return $response;
-	}
+        return $response;
+    }
 
-	public function getRepoInfoByRepoName( string $repoName ): array|bool
-	{
-		$request_uri = sprintf( 'https://api.github.com/repos/%s/%s/releases', $this->username, $repoName ); // Build URI
-		$response    = $this->getResponse( $request_uri );
+    public function getRepoInfoByRepoName(string $repoName): array|bool
+    {
+        $request_uri = sprintf('https://api.github.com/repos/%s/%s/releases', $this->username, $repoName); // Build URI
+        $response = $this->getResponse($request_uri);
 
-		if ( is_array( $response ) ) { // If it is an array
-			$response = current( $response ); // Get the first item
-		}
+        if (is_array($response)) { // If it is an array
+            $response = current($response); // Get the first item
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 }
--- cerberus-plugins-manager.php
+++ cerberus-plugins-manager.php
@@ -3,13 +3,15 @@
 /**
  * Plugin Name:       Cerberus Plugins Manager
  * Description:       Manages the cerberus plugins
- * Version:           1.0.6
+ * Version:           1.0.7
  * Requires at least: 5.7
  * Author:            Casey
  */
 
+use Cerberus\AdminPluginsManager\PluginUpdater;
 use Cerberus\AdminPluginsManager\SettingsPage;
 
 require_once "vendor/autoload.php";
 
-$settingsPage = new SettingsPage( __FILE__ );
+$settingsPage = new SettingsPage(__FILE__);
+$pluginUpdater = new PluginUpdater(__FILE__, 'cerberus-plugins-manager');
