72003fc2e4f8ea40e968c18ad3899a343294c5a3
casey <casey.reuter@trizelos.com>
2023-06-26 08:15:20 +0200

    fix: repo install

--- cerberus-plugins-manager.php
+++ cerberus-plugins-manager.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Cerberus Plugins Manager
  * Description:       Manages the cerberus plugins
- * Version:           1.0.6
+ * Version:           1.0.7
  * Requires at least: 5.7
  * Author:            Casey
  */
@@ -12,4 +12,4 @@ use Cerberus\AdminPluginsManager\SettingsPage;
 
 require_once "vendor/autoload.php";
 
-$settingsPage = new SettingsPage( __FILE__ );
+$settingsPage = new SettingsPage(__FILE__);
